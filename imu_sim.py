a95a74e4a8484f363105ffc6292db8f18069fa7e
YifeiNie <2096935953@qq.com>
2025-04-24 02:40:09 +0800

    first

---
+++ imu_sim.py
@@ -0,0 +1,123 @@
+import threading
+import yaml
+import time
+import torch
+import genesis as gs
+from genesis.utils.geom import quat_to_xyz, transform_by_quat, inv_quat, transform_quat_by_quat
+
+# NOTE :IMU is not an odomtry, only has anglear_velocity and linear_accerleration
+class IMU_sim:
+    def __init__(self, entity, env_num, yaml_path, device = torch.device("cuda")):
+        with open(yaml_path, "r") as file:
+            config = yaml.load(file, Loader = yaml.FullLoader)
+        self.device = device
+        self.entity = entity
+        self.env_num = env_num
+        
+        # self.last_body_pos = torch.zeros_like(self.body_pos)
+        # self.body_pos = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+
+        self.body_euler = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.body_linear_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.body_linear_acc = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)        
+        self.body_ang_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.last_body_linear_vel = torch.zeros_like(self.body_linear_vel)    # used to cal acc
+
+        self.body_quat = torch.zeros((self.env_num, 4), device=self.device, dtype=gs.tc_float)
+        self.body_quat_inv = torch.zeros((self.env_num, 4), device=self.device, dtype=gs.tc_float)
+
+        self.world_euler = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.world_linear_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.world_linear_acc = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.last_world_linear_vel = torch.zeros_like(self.body_linear_vel)
+        self.world_ang_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.last_time = time.perf_counter()
+
+
+        # for Betaflight
+        # gyro_sample_freq = 8kHz, gyro_filtered_freq = gyro_sample_freq / pid_process_denom = 4kHz
+        # acc_sample_freq = 1kHz, acc_target_freq = acc_sample_freq / pid_process_denom = = 500Hz
+        # attitude estimate freq = 100Hz / imu_process_denom
+        # pid_process_denom can found by "get pid_process_denom" in cli in Betaflight configrator
+        # imu_process_denom can found by "get imu_process_denom" in cli in Betaflight configrator
+
+        self.gyro_update_freq = config.get("gyro_update_freq", 8000)   # default are the same as Betaflight, aborted!
+        self.acc_update_freq = config.get("acc_update_freq", 1000)  
+        self.att_update_freq = config.get("att_update_freq", 100)
+       
+        self.gyro_update_dT = 1 / self.gyro_update_freq
+        self.acc_update_dT = 1 / self.acc_update_freq
+        self.att_update_dT = 1 / self.att_update_freq
+
+        self.tasks = {
+            "gyro_update": {
+                "interval": 1.0 / self.gyro_update_freq,  # 8000Hz
+                "last_time": time.perf_counter(),
+                "func": self.gyro_update
+            },
+            "acc_update": {
+                "interval": 1.0 / self.acc_update_freq,   # 1000Hz
+                "last_time": time.perf_counter(),
+                "func": self.acc_update
+            },
+            "att_update": {
+                "interval": 1.0 / self.att_update_freq,   # 100Hz
+                "last_time": time.perf_counter(),
+                "func": self.att_update
+            }
+        }
+
+    def cal_cur_quat(self):
+        self.body_quat[:] = self.entity.get_quat()
+        self.body_quat_inv[:] = inv_quat(self.body_quat)
+
+    def gyro_update(self):
+        self.cal_cur_quat(self)    # since gyro_update has the highest freq
+        cur_ang_vel = self.entity.get_ang()
+        self.body_ang_vel[:] = cur_ang_vel
+        self.world_ang_vel[:] = transform_by_quat(cur_ang_vel, self.body_quat_inv)
+
+    def acc_update(self, dT):
+        self.last_body_linear_vel[:] = self.body_linear_vel
+        body_linear_vel_temp = self.entity.get_vel()
+        self.body_linear_vel[:] = body_linear_vel_temp
+        self.world_linear_vel[:] = transform_by_quat(body_linear_vel_temp, self.body_quat_inv)
+
+        self.body_linear_acc[:] = (self.body_linear_vel - self.last_body_linear_vel) / dT
+        self.world_linear_acc[:] = transform_by_quat(self.body_linear_acc, self.body_quat_inv)
+
+
+    def att_update(self):
+        self.body_euler[:] = quat_to_xyz(self.body_quat)
+
+    # a simple scheduler to sim Betaflight
+    def imu_sim_loop(self):
+        while True:
+            now = time.perf_counter()
+            next_wakeup = float("inf")
+
+            for task_name, task in self.tasks.items():
+                elapsed = now - task["last_time"]
+                if elapsed >= task["interval"]:
+                    task["func"]()
+                    task["last_time"] = now
+                else:
+                    time_to_next = task["interval"] - elapsed
+                    next_wakeup = min(next_wakeup, time_to_next)
+
+            # sleep until the next nearest task
+            if next_wakeup > 0:
+                time.sleep(next_wakeup)
+
+    # NOTE Abort! since Genesis cannot update scene so quick
+    def start_imu_sim_thread(self):
+        t = threading.Thread(target=self.imu_sim_loop, daemon=True)
+        t.start()
+
+    # update imu every step (defalt in 60Hz)
+    def imu_update(self):
+        self.gyro_update(self)
+        self.acc_update(self, time.perf_counter() - self.last_time)
+        self.att_update(self)
+        self.last_time = time.perf_counter()
+        
---
+++ pid.py
@@ -0,0 +1,102 @@
+
+
+import yaml
+import torch
+import argparse
+import genesis as gs
+import time
+import threading
+from mavlink import rc_command
+
+
+class PIDcontroller:
+    def __init__(self, env_num, rc_command, imu_sim, yaml_path, device = torch.device("cuda")):
+        with open(yaml_path, "r") as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+        self.rc_command = rc_command
+        self.device = device
+        self.env_num = env_num
+
+        self.imu = imu_sim
+        # Shape: (n, 3)
+        self.kp = torch.tensor([config.get("kp_r", 0.0), config.get("kp_p", 0.0), config.get("kp_y", 0.0)], 
+                                device=self.device).unsqueeze(1).repeat(self.env_num, 1)  
+        self.ki = torch.tensor([config.get("ki_r", 0.0), config.get("ki_p", 0.0), config.get("ki_y", 0.0)], 
+                                device=self.device).unsqueeze(1).repeat(self.env_num, 1)
+        self.kd = torch.tensor([config.get("kd_r", 0.0), config.get("kd_p", 0.0), config.get("kd_y", 0.0)], 
+                                device=self.device).unsqueeze(1).repeat(self.env_num, 1)
+        self.kf = torch.tensor([config.get("kf_r", 0.0), config.get("kf_p", 0.0), config.get("kf_y", 0.0)], 
+                                device=self.device).unsqueeze(1).repeat(self.env_num, 1)
+
+        # self.entity_inertia = torch.tensor(config['inertia_matrix'], dtype=gs.tc_float)
+        
+        self.pid_freq = config.get("pid_exec_freq", 60)     # the same as gyro_update_freq
+        self.base_rpm = config.get("base_rpm", 10000)
+        self.dT = 1 / self.pid_freq
+        self.tpa_factor = 1
+
+        
+        self.last_body_ang_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        # self.last_body_ang = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        # self.last_body_linear_vel = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        # self.last_body_acc = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+
+        self.angle_rate_error = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.angle_error = torch.zeros_like(self.angle_rate_error)
+
+        self.P_term = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.I_term = torch.zeros_like(self.P_term)
+        self.D_term = torch.zeros_like(self.P_term)
+        self.F_term = torch.zeros_like(self.P_term)
+
+        self.body_set_point = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+        self.pid_output = torch.zeros((self.env_num, 3), device=self.device, dtype=gs.tc_float)
+
+    def mixer(self, thrust, roll, pitch, yaw, x_vel, y_vel) -> torch.Tensor:
+        M1 = self.base_rpm + (thrust - roll - pitch - yaw)
+        M2 = self.base_rpm + (thrust - roll + pitch + yaw)
+        M3 = self.base_rpm + (thrust + roll + pitch - yaw)
+        M4 = self.base_rpm + (thrust + roll - pitch + yaw)
+        return torch.Tensor([M1, M2, M3, M4])
+
+    def pid_update_TpaFactor(self):
+        if (rc_command["throttle"] > 0.35):       # 0.35 is the tpa_breakpoint, the same as Betaflight, 
+            if (rc_command["throttle"] < 1.0): 
+                tpa_rate *= (rc_command["throttle"] - 0.35) / (1.0 - 0.35);            
+            else:
+                tpa_rate = 0.0
+            
+            self.tpa_factor[:] = 1.0 - tpa_rate
+
+    def pid_loop(self):
+        self.imu.imu_update()
+        self.pid_update_TpaFactor()
+        if self.rc_command["angle"] == 0:         # angle mode
+            self.angle_controller(self)
+        elif self.rc_command["angle"] == 1:       # angle rate mode
+            self.angle_rate_controller(self)
+        else:                                     # undifined
+            print("undifined mode, do nothing!!")
+
+    def angle_rate_controller(self):  # use previous-D-term PID controller
+        self.body_set_point[:] = torch.tensor(list(rc_command.values())[:4]).repeat(self.env_num, 1)      # index 1:roll, 2:pitch, 3:yaw, 4:throttle
+        cur_angle_rate_error = self.body_set_point[:,0:2] - self.imu.body_ang_vel
+        self.P_term[:] = (cur_angle_rate_error * self.kp) * self.tpa_factor
+        self.I_term[:] = self.I_term + cur_angle_rate_error * self.ki
+        self.D_term[:] = (self.last_body_ang_vel - self.imu.body_ang_vel) * self.kd * self.tpa_factor    
+        # TODO feedforward term 
+        self.last_body_ang_vel[:] = self.imu.body_ang_vel
+        self.pid_output[:] = self.P_term + self.I_term + self.D_term
+        
+    # def angle_rate_controller(self):   
+
+
+
+
+
+
+
+
+
+
+    # def update_tpaFactor(self):
