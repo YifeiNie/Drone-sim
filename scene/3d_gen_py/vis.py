92b13c534a6ac950e2f987ed63dcf69d8e2f2e3e
YifeiNie <2096935953@qq.com>
2025-07-03 13:44:47 +0800

    cal entity dis not ready

--- scene/3d_gen_py/vis.py
+++ scene/3d_gen_py/vis.py
@@ -20,7 +20,6 @@ def get_obj_bbox_dims(filepath):
         "max_xyz": max_bound.tolist()
     }
 
-
 def visualize_models(root_path):
     for dirpath, _, filenames in os.walk(root_path):
         for filename in filenames:
@@ -28,9 +27,25 @@ def visualize_models(root_path):
                 filepath = os.path.join(dirpath, filename)
                 print(f"Loading {filepath}")
                 mesh = o3d.io.read_triangle_mesh(filepath)
+                # mesh.triangle_uvs = o3d.utility.Vector2dVector([])
+
+                # ======================================================================
+                vertices = np.asarray(mesh.vertices)
+                print(len(mesh.vertices))
+                # 设置阈值
+                z_threshold = 0.0 
+                filtered_vertices = vertices[vertices[:, 1] > z_threshold]
+                filtered_mesh = mesh.select_by_index(np.where(vertices[:, 2] > z_threshold)[0])
+                dec_mesh = filtered_mesh.simplify_quadric_decimation(target_number_of_triangles=5000)
+                unfiltered_mesh = mesh.select_by_index(np.where(vertices[:, 2] <= z_threshold)[0])
+                combined_mesh = dec_mesh + unfiltered_mesh
+                print(len(combined_mesh.vertices))
+
+                # ======================================================================
                 if not mesh.is_empty():
-                    # print(get_obj_bbox_dims(filepath))
-                    o3d.visualization.draw_geometries([mesh])
+                    print(get_obj_bbox_dims(filepath))
+                    o3d.visualization.draw_geometries([combined_mesh])
+                    
 
 
 if __name__ == "__main__":
