68ba0663d9e416d0fe396845c6a759b1d14501b4
YifeiNie <2096935953@qq.com>
2025-07-29 22:59:07 +0800

    adapt cnn and recurrent ppo

--- algorithms/feature_extract/depth_net.py
+++ algorithms/feature_extract/depth_net.py
@@ -2,23 +2,22 @@ import torch
 from torch import nn
 import yaml
 from tensordict import TensorDict
-
+import torch.nn.utils.rnn as rnn_utils
 
 def g_decay(x, alpha):
     return x * alpha + x.detach() * (1 - alpha)
 
 class Depth(nn.Module):
-    def __init__(self, input_dim, output_dim=4):
+    def __init__(self, input_dim, output_dim=4, num_layers=1):
         super().__init__()
 
         # input dim, dict[str, tuple[int]]
         self.state_dim = input_dim["state"][0]      # quat, anglular rate, linear acceleration, last action
-        self.img_dim = input_dim["depth"]           # depth image
+        self.img_dim = input_dim["depth"]           # depth image, 12*16
 
-        # input dim, scaler
         self.output_dim = output_dim                # ctbr   
-
-        self.hx = None
+        self.hidden_states = None
+        self.num_layers = num_layers
         
         self.stem = nn.Sequential(
             nn.Conv2d(1, 32, 2, 2, bias=False),  # 1, 12, 16 -> 32, 6, 8
@@ -33,17 +32,55 @@ class Depth(nn.Module):
         self.v_proj = nn.Linear(self.state_dim, 192)   
         self.v_proj.weight.data.mul_(0.5)
 
-        self.gru = nn.GRUCell(192, 192)
+        # self.gru = nn.GRUCell(192, 192)
+        self.gru = nn.GRU(192, 192, num_layers=self.num_layers)
         self.fc = nn.Linear(192, self.output_dim, bias=False)
         self.fc.weight.data.mul_(0.01)
         self.act = nn.LeakyReLU(0.05)   # activate
 
-    def reset(self):
-        pass
+    def reset(self, dones=None, hidden_states=None):
+        if dones is None:  # reset all hidden states
+            if hidden_states is None:
+                self.hidden_states = None
+            else:
+                self.hidden_states = hidden_states
+        elif self.hidden_states is not None:  # reset hidden states of done environments
+            if hidden_states is None:
+                if isinstance(self.hidden_states, tuple):  # tuple in case of LSTM
+                    for hidden_states in self.hidden_states:
+                        hidden_states[..., dones == 1, :] = 0.0
+                else:
+                    self.hidden_states[..., dones == 1, :] = 0.0
+            else:
+                NotImplementedError(
+                    "Resetting hidden states of done environments with custom hidden states is not implemented"
+                )
 
-    def forward(self, obs):
-        img_feat = self.stem(obs["depth"])
-        x = self.act(img_feat + self.v_proj(obs["state"]))
-        # h_new = self.gru(x, hidden_state)
-        act = self.fc(self.act(x))
+    def forward(self, obs, hidden_states=None, masks=None):
+        batch_mode = masks is not None
+        # train rollout
+        if batch_mode:
+            T, B, C, H, W = obs["depth"].shape
+            depth_reshape = obs["depth"].permute(1, 0, 2, 3, 4).reshape(B * T, C, H, W)
+            state_reshape = obs["state"].permute(1, 0, 2).reshape(B * T, -1)
+
+            img_feat = self.stem(depth_reshape)  # (B*T, 192)
+            state_proj = self.v_proj(state_reshape)  # (B*T, 192)
+
+            x = self.act(img_feat + state_proj)      # (B*T, 192)
+            x_seq = x.reshape(T, B, -1)  # (B, T, 192)
+
+            out_seq, h_new = self.gru(x_seq, hidden_states)  # out_seq: (T, B, hidden_dim)
+            final_hidden = h_new[-1]  # (B, hidden_dim)
+
+            act = self.fc(self.act(final_hidden))  # (B, num_actions)
+
+        # inference
+        else:
+            img_feat = self.stem(obs["depth"])
+            x_tem = self.act(img_feat + self.v_proj(obs["state"]))     
+            _, self.hidden_states = self.gru(x_tem.unsqueeze(0), self.hidden_states)
+            act = self.fc(self.act(self.hidden_states)).squeeze(0)
+
         return act
+
