baa2898a3301ba3d8ca6df813c6d46cc40730871
YifeiNie <2096935953@qq.com>
2025-08-01 16:05:34 +0800

    fix heapq_ bugs

--- utils/heapq_.py
+++ utils/heapq_.py
@@ -7,38 +7,38 @@ class MultiEntityList:
         self.max_size = max_size
         self.lists = [[] for _ in range(num_envs)]  
 
-    def update(self, entity_idxs, new_values):
+    def update(self, entity_idxs, distances):
         """
         update all scenes
         entity_idxs: list/array/tensor of shape [n_envs]
-        new_values: list/array/tensor of shape [n_envs]
+        distances: list/array/tensor of shape [n_envs]
         """
         entity_idxs = self.ensure_array(entity_idxs)
-        new_values = self.ensure_array(new_values)
+        distances = self.ensure_array(distances)
 
         for i in range(self.num_envs):
             idx = int(entity_idxs)
-            val = float(new_values[i])
+            val = float(distances[i])
             self._update_single_env(i, idx, val)
 
-    def _update_single_env(self, env_idx, entity_idx, new_value):
+    def _update_single_env(self, env_idx, entity_idx, distance):
         heap = self.lists[env_idx]
 
         for i in range(len(heap)):
             if heap[i][1] == entity_idx:  
-                heap[i] = [new_value, entity_idx] 
+                heap[i] = [-distance, entity_idx] 
                 heapq.heapify(heap)
                 return
         if len(heap) >= self.max_size:
             heapq.heappop(heap)
 
-        heapq.heappush(heap, [new_value, entity_idx])
+        heapq.heappush(heap, [-distance, entity_idx])
 
     def print(self):
         for i, heap in enumerate(self.lists):
             print(f"Env #{i}:")
             for item in heap:
-                print(f"(entity idx:{item[1]}, dis: {item[0]:.4f})")
+                print(f"(entity idx:{item[1]}, dis: {-item[0]:.4f})")
             print("-----")
 
     def ensure_array(self, x):
