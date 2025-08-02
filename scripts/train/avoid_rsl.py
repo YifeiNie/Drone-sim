f497e2481f506fee6c41cc9246f3abc6e62e2d71
YifeiNie <2096935953@qq.com>
2025-08-02 18:39:32 +0800

    tem

--- scripts/train/avoid_rsl.py
+++ scripts/train/avoid_rsl.py
@@ -37,7 +37,7 @@ def main():
     genesis_env = Genesis_env(
         env_config = env_config, 
         controller_config = controller_config,
-        load_map = True, 
+        load_map = False, 
         use_rc = False,
         render_cam = True,
         show_viewer = True, 
